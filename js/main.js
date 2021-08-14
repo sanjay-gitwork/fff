35a8bf36d0a3f553cf888a3390558ad20027785c
sanjay kumar <sanjaykumarrofficial@gmail.com>
2021-08-14 18:59:06 +0530

    pagination icons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 
-
-
-
 (function($) {
 
     "use strict";
@@ -39,6 +36,29 @@ function closeNav() {
 
 
 
+
+
+
+$('.sort-head').click(function(){
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+})
+function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+    }
+}
+function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+
+
+
+jQuery('.table-fake').fakeTableSortable({
+  // options here
+});
 
 
 
@@ -132,8 +152,6 @@ function closeNav() {
         }
     }                        
     
-
-    // datatables pagination
     this.init = function() {
         var rows = document.getElementById(tableName).rows;
         var records = (rows.length - 1); 
